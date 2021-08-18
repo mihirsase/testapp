b38e21215c7627791b37c274a7d44de7c06bd821
mihirsase <mihir@rirev.com>
2021-08-18 18:27:49 +0530

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(TestApp());
+}
+
+class TestApp extends StatefulWidget {
+  const TestApp({Key? key}) : super(key: key);
+
+  @override
+  _TestAppState createState() => _TestAppState();
+}
+
+class _TestAppState extends State<TestApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(),
+    );
+  }
+}
+
+
